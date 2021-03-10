66b353ff291cadd9103e39fe9ede4cbecfb6f7f2
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-03-10 16:22:59 +0100

    completo

---
+++ main.js
@@ -0,0 +1,31 @@
+//ciclo for fino a 100
+for (var i = 0; i <= 100; i++) {
+
+  if (!(i%15)){
+    console.log("fizzbuzz")
+  } else if (!(i%3)){
+    console.log("fizz");
+  }
+    else if (!(i%5)){
+      console.log("buzz");
+  } else {
+    console.log(i);
+  }
+
+
+
+}
+
+//stampare fizz per i multipli di 3
+
+
+
+
+
+//stampare fizz per i multipli di 5
+
+
+
+
+
+//stampare fizzbuzz per i multipli di 5 e 3
